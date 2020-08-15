bcde008e3122fe73622c159ffba84f4ba0300d46
Yoni Keren <yoni.keren@gmail.com>
2020-08-15 03:46:40 -0700

    simple facebook login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
 import 'matching_screen.dart';
-
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as JSON;
 
 void main()  {
   runApp(MyApp());}
@@ -25,22 +26,57 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class LoginHome extends StatelessWidget {
+class LoginHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginHomeState();
+}}
+
+class _LoginHomeState extends State<LoginHome>{ //See https://codesundar.com/flutter-facebook-login/
+  final facebookLogin = FacebookLogin();
+  bool _isLoggedIn=false;
+  Map userProfile;
+  _loginWithFB() async{
+
+
+    final result = await facebookLogin.logIn(['public_profile']);
+
+    switch (result.status) {
+      case FacebookLoginStatus.loggedIn:
+        final token = result.accessToken.token;
+        final graphResponse = await http.get('https://graph.facebook.com/v2.12/me?fields=name,picture,email&access_token=$token');
+        final profile = JSON.jsonDecode(graphResponse.body);
+        print(profile);
+        setState(() {
+          userProfile = profile;
+          _isLoggedIn = true;
+        });
+        break;
+
+      case FacebookLoginStatus.cancelledByUser:
+        setState(() => _isLoggedIn = false );
+        break;
+      case FacebookLoginStatus.error:
+        setState(() => _isLoggedIn = false );
+        break;
+    }
+
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
         body:Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               OutlineButton(
-                child:Text('Click me!'),
+                child:Text('FaceBook Login'),
                 onPressed: (){
-                  Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => MatchingScreen(title: 'Flutter Demo Home Page')
-
-                  ));
+                  _loginWithFB();
+                  //Navigator.push(context, MaterialPageRoute(builder: (context) => MatchingScreen(title: 'Flutter Demo Home Page')));
                 },
               )
             ],
