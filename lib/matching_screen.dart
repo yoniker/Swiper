641830fb50b740daf8be7ad192d39073bd6e00a1
Yoni Keren <yoni.keren@gmail.com>
2020-08-16 16:10:26 -0700

    replace lock sign with a question mark

--- lib/matching_screen.dart
+++ lib/matching_screen.dart
@@ -1,3 +1,4 @@
+import 'package:betabeta/round_icon_button.dart';
 import 'package:betabeta/search_preferences_screen.dart';
 import 'package:betabeta/user_profile.dart';
 import 'package:betabeta/cards.dart';
@@ -134,9 +135,8 @@ class _MatchingScreenState extends State<MatchingScreen> {
                     widget.matchEngine.goToNextMatch();}
                 },
               ),
-              RoundIconButton.small(
-                icon: Icons.lock,
-                iconColor: Colors.purple,
+              RoundButton.small(
+                child:Text('?',style: TextStyle(color:Colors.grey,fontSize: 30,fontWeight: FontWeight.bold),),
                 onPressed: () {},
               ),
             ],
@@ -161,51 +161,4 @@ class _MatchingScreenState extends State<MatchingScreen> {
   }
 }
 
-class RoundIconButton extends StatelessWidget {
-  final IconData icon;
-  final Color iconColor;
-  final double size;
-  final VoidCallback onPressed;
 
-  RoundIconButton.large({
-    this.icon,
-    this.iconColor,
-    this.onPressed,
-  }) : size = 60.0;
-
-  RoundIconButton.small({
-    this.icon,
-    this.iconColor,
-    this.onPressed,
-  }) : size = 50.0;
-
-  RoundIconButton({
-    this.icon,
-    this.iconColor,
-    this.size,
-    this.onPressed,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: size,
-      height: size,
-      decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: Colors.white,
-          boxShadow: [
-            BoxShadow(color: const Color(0x11000000), blurRadius: 10.0),
-          ]),
-      child:  RawMaterialButton(
-        shape:  CircleBorder(),
-        elevation: 0.0,
-        child:  Icon(
-          icon,
-          color: iconColor,
-        ),
-        onPressed: onPressed,
-      ),
-    );
-  }
-}
---
+++ lib/round_icon_button.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class RoundIconButton extends StatelessWidget {
+  final IconData icon;
+  final Color iconColor;
+  final double size;
+  final VoidCallback onPressed;
+
+  RoundIconButton.large({
+    this.icon,
+    this.iconColor,
+    this.onPressed,
+  }) : size = 60.0;
+
+  RoundIconButton.small({
+    this.icon,
+    this.iconColor,
+    this.onPressed,
+  }) : size = 50.0;
+
+  RoundIconButton({
+    this.icon,
+    this.iconColor,
+    this.size,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(color: const Color(0x11000000), blurRadius: 10.0),
+          ]),
+      child:  RawMaterialButton(
+        shape:  CircleBorder(),
+        elevation: 0.0,
+        child:  Icon(
+          icon,
+          color: iconColor,
+        ),
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+class RoundButton extends StatelessWidget {
+  final Widget child;
+  final double size;
+  final VoidCallback onPressed;
+
+  RoundButton.large({
+    this.child,
+    this.onPressed,
+  }) : size = 60.0;
+
+  RoundButton.small({
+    this.child,
+    this.onPressed,
+  }) : size = 50.0;
+
+  RoundButton({
+    this.child,
+    this.size,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(color: const Color(0x11000000), blurRadius: 10.0),
+          ]),
+      child:  RawMaterialButton(
+        shape:  CircleBorder(),
+        elevation: 0.0,
+        child:  child,
+        onPressed: onPressed,
+      ),
+    );
+  }
+}
