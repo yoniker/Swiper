0a4434c9f8d1f1031b34fd8e54c9b16555aaca72
Yoni Keren <yoni.keren@yahoo.ca>
2021-06-02 18:27:45 +0300

    final mock UI chat version

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:betabeta/models/chat_message_model.dart';
+import 'package:betabeta/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget{
@@ -10,10 +12,109 @@ class _ChatScreenState extends State<ChatScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Chat Detail"),
+        appBar: CustomAppBar(
+          trailing: Icon(Icons.settings,color: Colors.black54,),
+          customTitleBuilder:
+              Container(
+                width: 200,
+                child: Row(children: [
+                  CircleAvatar(
+                    backgroundImage: NetworkImage("https://randomuser.me/api/portraits/men/5.jpg"),
+                    maxRadius: 20,
+                  ),
+                  SizedBox(width: 12,),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text("Kriss Benwat",style: TextStyle( fontSize: 16 ,fontWeight: FontWeight.w600),),
+                        SizedBox(height: 6,),
+                        Text("Online",style: TextStyle(color: Colors.grey.shade600, fontSize: 13),),
+                      ],
+                    ),
+                  ),
+
+                ],
+                ),
+              ),
+          hasTopPadding:true,
         ),
-        body: Container()
+        body: Stack(
+    children: <Widget>[
+      Container(
+        margin: EdgeInsets.only(bottom: 45.0),
+        child: ListView.builder(
+          itemCount: ChatMessage.mockMessages.length,
+          shrinkWrap: true,
+          padding: EdgeInsets.only(top: 10,bottom: 10),
+          physics: BouncingScrollPhysics(),
+          itemBuilder: (context, index){
+            return Container(
+              padding: EdgeInsets.only(left: 14,right: 14,top: 10,bottom: 10),
+              child: Align(
+                alignment: (ChatMessage.mockMessages[index].messageType == "receiver"?Alignment.topLeft:Alignment.topRight),
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: (ChatMessage.mockMessages[index].messageType  == "receiver"?Colors.grey.shade200:Colors.blue[200]),
+                  ),
+                  padding: EdgeInsets.all(16),
+                  child: Text(ChatMessage.mockMessages[index].messageContent, style: TextStyle(fontSize: 15),),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+
+
+    Align(
+      alignment: Alignment.bottomLeft,
+      child: Container(
+        padding: EdgeInsets.only(left: 10,bottom: 10,top: 10),
+        height: 60,
+        width: double.infinity,
+        color: Colors.white,
+        child: Row(
+          children: <Widget>[
+            GestureDetector(
+              onTap: (){
+              },
+              child: Container(
+                height: 30,
+                width: 30,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Icon(Icons.add, color: Colors.white, size: 20, ),
+              ),
+            ),
+            SizedBox(width: 15,),
+            Expanded(
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: "Write message...",
+                    hintStyle: TextStyle(color: Colors.black54),
+                    border: InputBorder.none
+                ),
+              ),
+            ),
+            SizedBox(width: 15,),
+            FloatingActionButton(
+              onPressed: (){},
+              child: Icon(Icons.send,color: Colors.white,size: 18,),
+              backgroundColor: Colors.blue,
+              elevation: 0,
+            ),
+          ],
+
+        ),
+      ),
+    ),
+    ],
+    ),
     );
   }
 }
