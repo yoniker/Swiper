9ccce3a3aef3804efc9dd692e9ee1dd4ac2a98e3
Yoni Keren <yoni.keren@yahoo.ca>
2021-07-22 01:16:28 +0300

    Larger and filled settings icon at matching screen

--- lib/screens/match_screen.dart
+++ lib/screens/match_screen.dart
@@ -174,8 +174,8 @@ class _MatchScreenState extends State<MatchScreen>
             ),
             showAppLogo: true,
             hasBackButton: false,
-            trailing: GlobalWidgets.assetImageToIcon(
-              BetaIconPaths.settingsBarIcon,
+            trailing: GestureDetector(
+              child: Icon(Icons.settings,size:35,color:colorBlend02),
               onTap: () async {
                 // hide the overlay.
                 setMatchCardVisibility(false);
