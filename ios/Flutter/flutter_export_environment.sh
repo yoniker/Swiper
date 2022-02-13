56098e8d3fc71fb1c4b2be47e1fb7b7fc243554d
Yoni Keren <yoni.keren@yahoo.ca>
2022-02-13 18:10:35 +0200

    env

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/yoni/snap/flutter/common/flutter"
+export "FLUTTER_ROOT=/home/yoni/Software/flutter"
 export "FLUTTER_APPLICATION_PATH=/home/yoni/Projects/swiper"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
